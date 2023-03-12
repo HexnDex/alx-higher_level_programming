35c8d60a06a4fd1bd8fd5132a63f43b69b55c6f7
HexnDex <eowusularbi@gmail.com>
2023-03-12 11:56:11 -0700

    corrections

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            num = 32
-        else:
-            num = 0
-        print("{:c}".format(ord(str[i]) - num), end='')
+    for c in str:
+        isupper = ord(c) >= ord('a') and ord(c) <= ord('z')
+        if isupper:
+            # difference between 'a' and 'A'
+            diff = ord('A') - ord('a')
+            c = chr(ord(c) + diff)
+        print("{}".format(c), end='')
     print()
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        l_digit = number % 10
-    else:
-        l_digit = number % -10
-        l_digit *= -1
-
-    print("{:d}".format(l_digit), end='')
-    return (l_digit)
+    last_digit = abs(number) % 10
+    return print(last_digit, end='') or last_digit
